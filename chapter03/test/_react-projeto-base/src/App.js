cc0df0dfa6698ad7d33fc7faa77b5de970713a7e
rev0lts <luisvinicius0906@gmail.com>
2020-06-19 16:51:33 -0300

    delete tests

--- chapter03/test/_react-projeto-base/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
-
-export default class App extends Component {
-  render() {
-    return <ProjetoBase />;
-  }
-}
