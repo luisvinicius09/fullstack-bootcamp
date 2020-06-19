d6eb4c637812cc3ff18de23f4d66a4508680ff96
rev0lts <luisvinicius0906@gmail.com>
2020-06-19 15:52:25 -0300

    commit gE01

---
+++ chapter03/guidedExercice02/search-countries/src/components/countries/Country.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Country extends Component {
+    render() {
+        const {country} = this.props;
+        return (
+            <div>
+                {country.name}
+            </div>
+        )
+    }
+}
---
+++ chapter03/guidedExercice02/search-countries/src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {formatNumber} from '../helpers/formatHelpers'
+import css from './header.module.css';
+
+export default class Header extends Component {
+    handleInputChange = (event) => {
+        const newText = event.target.value;
+
+        this.props.onChangeFilter(newText);
+    }
+    render() {
+        const {filter, countryCount, totalPopulation} = this.props;
+        return (
+            <div className={css.flexRow}>
+                <input type="text" value={filter} onChange={this.handleInputChange} /> |
+        <span className={css.countries}>Countries: <strong>{countryCount}</strong></span> |
+        <span className={css.population}>Population: <strong>{formatNumber(totalPopulation)}</strong></span>
+            </div>
+        )
+    }
+}
